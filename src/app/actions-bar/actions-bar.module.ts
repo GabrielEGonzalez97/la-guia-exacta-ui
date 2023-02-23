2bf23c693aa2e35154a66f9f715cdc0b580726c8
Gabriel Gonzalez <gabrielenrique.gonzalez97@gmail.com>
2023-02-22 23:33:41 -0300

    Improvements in the ActionsBar component using the HeaderModule of Carbon

--- src/app/actions-bar/actions-bar.module.ts
+++ src/app/actions-bar/actions-bar.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ActionsBarComponent } from './actions-bar.component';
 import { ActionBarElementModule } from './action-bar-element/action-bar-element.module';
+import { HeaderModule } from 'carbon-components-angular';
 
 const COMPONENT_IMPORTS = [ActionBarElementModule];
 
 @NgModule({
   declarations: [ActionsBarComponent],
-  imports: [COMPONENT_IMPORTS],
+  imports: [COMPONENT_IMPORTS, HeaderModule],
   providers: [],
   exports: [ActionsBarComponent],
 })
