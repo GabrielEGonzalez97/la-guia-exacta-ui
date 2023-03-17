65e280748b3c5f44032468a0e61fb2a520481ded
Gabriel Gonzalez <gabrielenrique.gonzalez97@gmail.com>
2023-03-17 00:12:06 -0300

    Added a HttpService service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { PlaceholderModule } from 'carbon-components-angular';
@@ -14,6 +15,7 @@ import { NavbarModule } from './navbar/navbar.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     ActionsBarModule,
     CatedrasModule,
     FinalesModule,
---
+++ src/app/services/http.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IGoogleDriveFolderInformation } from '../common/interfaces';
+import { IWithState, UtilsService } from './utils.service';
+
+const API_URL = 'http://127.0.0.1:5000';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpService {
+  constructor(private http: HttpClient, private utilsService: UtilsService) {}
+
+  public getFilesFromFolderWithinASubject(
+    subjectName: string,
+    folderToSearch: string
+  ): Observable<IWithState<IGoogleDriveFolderInformation[]>> {
+    return this.getWithState<IGoogleDriveFolderInformation[]>(
+      API_URL +
+        `/getFilesFromFolderWithinASubject/${subjectName}/${folderToSearch}`
+    );
+  }
+
+  public getFileById(fileId: string): Observable<IWithState<ArrayBuffer>> {
+    return this.getWithState<ArrayBuffer>(
+      API_URL + `/getFileById/${fileId}`,
+      'arraybuffer'
+    );
+  }
+
+  private getWithState = <T>(url: string, responseType?, params?: HttpParams) =>
+    this.utilsService.withState(
+      this.http.get<T>(url, { params, responseType })
+    );
+}
