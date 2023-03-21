bd00a22cb8686cb364c3b0855e5f0c080248edac
GabrielEGonzalez97 <gabrielenrique.gonzalez97@gmail.com>
2023-03-21 18:42:00 -0300

    Added a IngenieriaDeSistemasPlanDeEstudiosComponent component

--- src/app/ingenieria-de-sistemas/ingenieria-de-sistemas.module.ts
+++ src/app/ingenieria-de-sistemas/ingenieria-de-sistemas.module.ts
@@ -9,14 +9,20 @@ import { IngenieriaDeSistemasComponent } from './ingenieria-de-sistemas.componen
 import { IngenieriaDeSistemasDescripcionComponent } from './ingenieria-de-sistemas-descripcion/ingenieria-de-sistemas-descripcion.component';
 import { CatedraModalWindowComponent } from '../catedras/catedra-modal-window/catedra-modal-window.component';
 import { CatedraModule } from '../catedras/catedra/catedra.module';
+import { IngenieriaDeSistemasPlanDeEstudiosComponent } from './ingenieria-de-sistemas-plan-de-estudios/ingenieria-de-sistemas-plan-de-estudios.component';
 
 const CARBON_IMPORTS = [GridModule, ModalModule, TilesModule];
+
+const COMPONENT_IMPORTS = [
+  IngenieriaDeSistemasDescripcionComponent,
+  IngenieriaDeSistemasPlanDeEstudiosComponent,
+];
 
 @NgModule({
   declarations: [
     IngenieriaDeSistemasComponent,
-    IngenieriaDeSistemasDescripcionComponent,
     CatedraModalWindowComponent,
+    COMPONENT_IMPORTS,
   ],
   imports: [CommonModule, CARBON_IMPORTS, CatedraModule],
   providers: [],
