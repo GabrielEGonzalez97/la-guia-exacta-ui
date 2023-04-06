7c1965f63bedebe1b5b5edfd29aae116778b4c10
GabrielEGonzalez97 <gabrielenrique.gonzalez97@gmail.com>
2023-04-06 19:45:36 -0300

    Hide right panel after click outside of them

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { UtilsService } from '../services/utils.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  host: {
+    '(document:click)': 'onClickOutsideRightPanel($event)',
+  },
 })
 export class NavbarComponent implements OnInit {
   public isRightPanelVisible: boolean = false;
 
-  constructor(private utilsService: UtilsService) {}
+  constructor(
+    private elementRef: ElementRef,
+    private utilsService: UtilsService
+  ) {}
 
   public ngOnInit(): void {}
 
@@ -19,5 +25,11 @@ export class NavbarComponent implements OnInit {
 
   public navigateTo(route: string): void {
     this.utilsService.navigateTo(route);
+  }
+
+  public onClickOutsideRightPanel(event: any): void {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.isRightPanelVisible = false;
+    }
   }
 }
