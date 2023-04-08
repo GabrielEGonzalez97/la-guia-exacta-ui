9537cac445685f46b8e1af91f8d961da9aa13843
GabrielEGonzalez97 <gabrielenrique.gonzalez97@gmail.com>
2023-04-08 18:05:43 -0300

    Added chevron icons on the ingenieria de sistemas tiles

--- src/app/ingenieria-de-sistemas/ingenieria-de-sistemas.module.ts
+++ src/app/ingenieria-de-sistemas/ingenieria-de-sistemas.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import {
   ButtonModule,
   GridModule,
+  IconModule,
+  IconService,
   ModalModule,
   TableModule,
   TilesModule,
@@ -14,9 +16,13 @@ import { CatedraModule } from '../catedras/catedra/catedra.module';
 import { IngenieriaDeSistemasPlanDeEstudiosComponent } from './ingenieria-de-sistemas-plan-de-estudios/ingenieria-de-sistemas-plan-de-estudios.component';
 import { ActionBarElementModule } from '../actions-bar/action-bar-element/action-bar-element.module';
 
+import ChevronDown32 from '@carbon/icons/es/chevron--down/32';
+import ChevronUp32 from '@carbon/icons/es/chevron--up/32';
+
 const CARBON_IMPORTS = [
   ButtonModule,
   GridModule,
+  IconModule,
   ModalModule,
   TableModule,
   TilesModule,
@@ -43,5 +49,7 @@ const COMPONENT_IMPORTS = [
   exports: [IngenieriaDeSistemasComponent],
 })
 export class IngenieriaDeSistemasModule {
-  constructor() {}
+  constructor(protected iconService: IconService) {
+    iconService.registerAll([ChevronDown32, ChevronUp32]);
+  }
 }
