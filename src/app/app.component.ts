742b1058027d5bf21c5694b9ab23241966ed2e01
GabrielEGonzalez97 <gabrielenrique.gonzalez97@gmail.com>
2023-04-17 20:02:23 -0300

    Added google analytics to all the views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ingenieria-sistemas-app';
+
+  constructor(public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-QD8PY9PFZ8', {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    });
+  }
 }
