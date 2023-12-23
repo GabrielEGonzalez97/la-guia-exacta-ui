152a862afc69d61f8589ad3b59eb38499da74017
GabrielEGonzalez97 <gabrielenrique.gonzalez97@gmail.com>
2023-12-23 15:42:44 -0300

    Added matrix multiplication

--- src/app/herramientas/Parser/Terceto/Terceto.ts
+++ src/app/herramientas/Parser/Terceto/Terceto.ts
@@ -43,14 +43,23 @@ export class Terceto extends TercetoAbstracto {
     const numerator: number = fraction.n;
     const denominator: number = fraction.d;
 
-    const fractionString: string = `${numerator}\\over${denominator}`;
+    const minusSign: string = decimal < 0 ? '-' : '';
+    const fractionString: string = `${minusSign}${numerator}\\over${denominator}`;
 
     if (denominator > 1) {
       return fractionString;
     }
 
-    const minusSign: string = decimal < 0 ? '-' : '';
     return `${minusSign}${numerator}`;
+  }
+
+  private getMatrixCellValue(cellValue: IMatrixElement): number {
+    if (cellValue.value.includes('/')) {
+      const divisionParts: string[] = cellValue.value.split('/');
+      return Number(divisionParts[0]) / Number(divisionParts[1]);
+    } else {
+      return Number(cellValue.value);
+    }
   }
 
   public override getResultado(): number | IMatrixElement[][] {
@@ -90,22 +99,8 @@ export class Terceto extends TercetoAbstracto {
         for (let i: number = 0; i < numberOfRowsOfMatrix1; i++) {
           const filaResultado: IMatrixElement[] = [];
           for (let j: number = 0; j < numberOfColumnsOfMatrix1; j++) {
-            let valorMatriz1: number = 0;
-            if (matrix1[i][j].value.includes('/')) {
-              const divisionParts: string[] = matrix1[i][j].value.split('/');
-              valorMatriz1 =
-                Number(divisionParts[0]) / Number(divisionParts[1]);
-            } else {
-              valorMatriz1 = Number(matrix1[i][j].value);
-            }
-            let valorMatriz2: number = 0;
-            if (matrix2[i][j].value.includes('/')) {
-              const divisionParts: string[] = matrix2[i][j].value.split('/');
-              valorMatriz2 =
-                Number(divisionParts[0]) / Number(divisionParts[1]);
-            } else {
-              valorMatriz2 = Number(matrix2[i][j].value);
-            }
+            const valorMatriz1: number = this.getMatrixCellValue(matrix1[i][j]);
+            const valorMatriz2: number = this.getMatrixCellValue(matrix2[i][j]);
             const suma: number = valorMatriz1 + valorMatriz2;
             filaResultado.push({ value: this.decimalToFraction(suma) });
           }
@@ -148,10 +143,10 @@ export class Terceto extends TercetoAbstracto {
         for (let i: number = 0; i < numberOfRowsOfMatrix1; i++) {
           const filaResultado: IMatrixElement[] = [];
           for (let j: number = 0; j < numberOfColumnsOfMatrix1; j++) {
-            const valorMatriz1: number = Number(matrix1[i][j].value);
-            const valorMatriz2: number = Number(matrix2[i][j].value);
+            const valorMatriz1: number = this.getMatrixCellValue(matrix1[i][j]);
+            const valorMatriz2: number = this.getMatrixCellValue(matrix2[i][j]);
             const resta: number = valorMatriz1 - valorMatriz2;
-            filaResultado.push({ value: resta.toString() });
+            filaResultado.push({ value: this.decimalToFraction(resta) });
           }
           resultado.push(filaResultado);
         }
@@ -164,6 +159,52 @@ export class Terceto extends TercetoAbstracto {
           Number(this.operand1.getResultado()) *
           Number(this.operand2.getResultado())
         );
+      } else if (this.evaluateOperandsTypes(MATRIX_TYPE, MATRIX_TYPE)) {
+        const matrix1: IMatrixElement[][] =
+          this.operand1.getResultado() as IMatrixElement[][];
+        const matrix2: IMatrixElement[][] =
+          this.operand2.getResultado() as IMatrixElement[][];
+        const numberOfRowsOfMatrix1: number = matrix1.length;
+        const numberOfColumnsOfMatrix1: number = matrix1[0].length;
+        const numberOfRowsOfMatrix2: number = matrix2.length;
+        const numberOfColumnsOfMatrix2: number = matrix2[0].length;
+
+        if (numberOfColumnsOfMatrix1 !== numberOfRowsOfMatrix2) {
+          throw new Error(
+            'El número de columnas de la primera matriz debe ser igual al número de filas de la segunda matriz para poder multiplicarlas.'
+          );
+        }
+
+        const resultado: IMatrixElement[][] = Array.from(
+          { length: numberOfRowsOfMatrix1 },
+          () =>
+            Array(numberOfColumnsOfMatrix2)
+              .fill(undefined)
+              .map(() => ({ value: '0' }))
+        );
+
+        for (let i = 0; i < numberOfRowsOfMatrix1; i++) {
+          for (let j = 0; j < numberOfColumnsOfMatrix2; j++) {
+            let suma: number = 0;
+            for (let k = 0; k < numberOfColumnsOfMatrix1; k++) {
+              const valorMatriz1: number = this.getMatrixCellValue(
+                matrix1[i][k]
+              );
+              const valorMatriz2: number = this.getMatrixCellValue(
+                matrix2[k][j]
+              );
+
+              suma += valorMatriz1 * valorMatriz2;
+            }
+            resultado[i][j].value = this.decimalToFraction(
+              this.getMatrixCellValue({
+                value: suma.toString(),
+              })
+            );
+          }
+        }
+
+        return resultado;
       }
     } else if (this.operator === '/') {
       if (this.evaluateOperandsTypes(NUMBER_TYPE, NUMBER_TYPE)) {
@@ -207,6 +248,8 @@ export class Terceto extends TercetoAbstracto {
     } else if (this.operator === '*') {
       if (this.evaluateOperandsTypes(NUMBER_TYPE, NUMBER_TYPE)) {
         return NUMBER_TYPE;
+      } else if (this.evaluateOperandsTypes(MATRIX_TYPE, MATRIX_TYPE)) {
+        return MATRIX_TYPE;
       }
     } else if (this.operator === '/') {
       if (this.evaluateOperandsTypes(NUMBER_TYPE, NUMBER_TYPE)) {
